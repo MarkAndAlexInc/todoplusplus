d7c707889fd3edfd48eb3786a4577d4bf5b21a32
janssea <alex.janssen@uberflip.com>
2018-09-25 19:46:12 -0400

    master: skeleton of to do app

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class ToDo {
+    public:
+        char title[10];
+        bool done;
+        ToDo();
+        ToDo(string title, bool done);
+};
+
+vector<ToDo> toDoArr;
 
 int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
+    ToDo myFirstToDo("my to do", false);
+    toDoArr.push_back(myFirstToDo);
+    cout << toDoArr[0].title << endl;
 }
