d00d864b2c5eceaa51c24d15ca6f1da8feef9399
wilkinm <mark.wilkins@uberflip.com>
2018-09-25 21:37:26 -0400

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,37 @@ ToDo::ToDo (string t, bool d) {
 
 vector<ToDo> toDoArr;
 
+void listToDos() {
+    cout << endl;
+    cout << "** Your TODO List **" << endl;
+    cout << endl;
+
+    if(toDoArr.size() < 1) {
+        cout << "No todos!" << endl;
+    }
+    
+    for (size_t i = 0; i < toDoArr.size(); i++) {
+        cout << "TODO #" << (i + 1) << ":" << endl;
+        cout << "   title: " << toDoArr[i].title << endl;
+        cout << "   complete: " << (toDoArr[i].done ? "Yes" : "No") << endl;
+        cout << endl;
+    }
+}
+
+void addToDo(string title) {
+    ToDo newToDo(title, false);
+    toDoArr.push_back(newToDo);
+}
+
+bool removeToDo(int userSelection) {
+    int idOfTodoToRemove = userSelection - 1;
+    // Find todo, return true if found and removed false 
+}
+
 int main() {
-    ToDo myFirstToDo ("hello", true);
-    toDoArr.push_back(myFirstToDo);
-    cout << toDoArr[0].title << endl;
+
+    addToDo("My first todo!");
+    addToDo("My second todo!");
+
+    listToDos();
 }
