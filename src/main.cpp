b08abdb2bee5c0d2e7e0650b05d98f64eaf8fd04
wilkinm <mark.wilkins@uberflip.com>
2018-09-25 21:08:29 -0400

    fix class constructor

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,21 @@ using namespace std;
 
 class ToDo {
     public:
-        char title[10];
+        string title;
         bool done;
         ToDo();
-        ToDo(string title, bool done);
+        ToDo(string, bool);       
 };
+
+ToDo::ToDo (string t, bool d) {
+    title = t;
+    done = d;
+}
 
 vector<ToDo> toDoArr;
 
 int main() {
-    ToDo myFirstToDo("my to do", false);
+    ToDo myFirstToDo ("hello", true);
     toDoArr.push_back(myFirstToDo);
     cout << toDoArr[0].title << endl;
 }
